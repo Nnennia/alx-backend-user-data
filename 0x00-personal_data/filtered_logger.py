d08e7934c7564ee0ab2f02d0bc7b5ce94057b8fb
Nnennia <nnenniaatuanya@gmail.com>
2023-05-24 18:20:21 +0100

    commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -57,6 +57,7 @@ def get_logger():
 
 
 def get_db():
+    """Connect to a db to read a db table"""
     username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
